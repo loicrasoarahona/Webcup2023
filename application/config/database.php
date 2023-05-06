bc569797115d362ad5fbb5105f3d6bea83a7073d
syraxxxx <rsitraka181@gmail.com>
2023-05-07 02:48:32 +0300

    a propos , login , page accueil ,  inscription

--- application/config/database.php
+++ application/config/database.php
@@ -79,7 +79,7 @@ $db['default'] = array(
 	'username' => 'root',
 	'password' => '',
 	'database' => 'smartit_2023',
-	'dbdriver' => 'mysqli',
+ 	'dbdriver' => 'mysqli',
 	'dbprefix' => '',
 	'pconnect' => FALSE,
 	'db_debug' => (ENVIRONMENT !== 'production'),
---
+++ application/views/about.php
@@ -0,0 +1,272 @@
+<header class="header-pages mb-section header-padding-top dsn-container background-main">
+    <div class="box-title ">
+        <h1 class="title">
+            <span class="letter-stroke">A Propos </span> <br>
+            <span class="line-bg-left pl-80">de l'institut International des rêves</span>
+        </h1>
+        <div class="mt-30 d-flex w-100 justify-content-end">
+            <p class="border-left pl-20 max-w570">
+                Vous avez plusieurs choix pour nous decrire votre rêve , que vous nous le racontez directement ,
+                choissisez des questions à choix , ou bien les illustrer par des images , nous sommes là pour vous prédire votre avenir.</p>
+        </div>
+    </div>
+</header>
+
+<!-- ========== Service Section ========== -->
+<section class="services pb-section background-section dsn-container p-relative " data-dsn-title="Our Service">
+    <div class="section-title">
+        <span class="tag-heading p-10 mb-15 background-main heading-color">Pourquoi nous ?</span>
+        <h2 class="heading-h2">Nous combinons recherche, <br> psychologie et numérique</h2>
+    </div>
+    <div class="d-grid dsn-isotope grid-md-2 grid-lg-3" data-dsn-item=".service-item">
+        <div class="service-item p-relative border">
+            <div class="service-item-inner style-box">
+                <div class="icon">
+                    <img src=<?php echo site_url("assets/img/services/mobile.svg") ?> alt="">
+                </div>
+                <div class="content-box">
+                    <h4 class="title-block mt-20">Transcription de vos rêves</h4>
+                    <p class="description mt-20">Racontez nous vos rêves les plus en détails possible afin que nous puissions mieux les comprendre
+                        pour pouvoir prédire votre avenir
+                    </p>
+                </div>
+            </div>
+        </div>
+        <div class="service-item p-relative border">
+            <div class="service-item-inner style-box">
+                <div class="icon">
+                    <img src=<?php echo site_url("assets/img/services/development.svg") ?> alt="">
+                </div>
+                <div class="content-box">
+                    <h4 class="title-block mt-20">Par Options</h4>
+                    <p class="description mt-20">Un large choix de questions pour vous pour que nous puissions en savoir plus sur vot
+                    </p>
+                </div>
+            </div>
+        </div>
+        <div class="service-item p-relative border">
+            <div class="service-item-inner style-box">
+                <div class="icon">
+                    <img src=<?php echo site_url("assets/img/services/creative.svg") ?> alt="">
+                </div>
+                <div class="content-box">
+                    <h4 class="title-block mt-20">Par images</h4>
+                    <p class="description mt-20">Une Interpretation différentes pour illuster le fonds de vos rêves .
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<!-- ========== End Service Section ========== -->
+
+<!-- ========== testimonials 2 ========== -->
+<section class="testimonials testimonials-two dsn-container p-relative mb-section dsn-swiper" data-dsn-title="Clients See">
+    <div class="section-title">
+        <span class="tag-heading p-10 mb-15 background-section heading-color">
+            Clients
+        </span>
+        <h2 class="heading-h2">Retours <br> de nos clients.</h2>
+    </div>
+
+    <div class="testimonial-inner ">
+        <div class="swiper-container">
+            <div class="swiper-wrapper">
+                <div class="swiper-slide testimonial-item">
+                    <i class="fas fa-quote-left"></i>
+                    <div class="testimonial-content">
+                        <p class="max-w750 p-large">"The designer is just amazing! Very good quality
+                            and
+                            taste.
+                            This template is incredible beautiful. The overall
+                            impression
+                            is more than good.The support cannot be better. I wish
+                            the
+                            team all success!"</p>
+                    </div>
+
+                    <div class="testimonial-author d-flex align-items-center">
+                        <div class="author">
+                            <img src=<?php echo site_url("assets/img/team/1.jpg") ?> alt="">
+                        </div>
+                        <div class="author-text">
+                            <h4>HellstarWorks</h4>
+                            <h5>Envato Customer</h5>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="swiper-slide testimonial-item">
+                    <i class="fas fa-quote-left"></i>
+                    <div class="testimonial-content">
+                        <p class="max-w750 p-large">"First of all, I really enjoyed setting this up.
+                            What a
+                            great
+                            layout and design. The support is fast and high quality.
+                            They
+                            really made an effort to make sure I got the help I
+                            needed.
+                            keep in mind that it is not only a great design, but
+                            also it
+                            is easy to change it up with little effort. Great Job!"</p>
+                    </div>
+
+                    <div class="testimonial-author d-flex align-items-center">
+                        <div class="author">
+                            <img src=<?php echo site_url("assets/img/team/2.jpg") ?> alt="">
+                        </div>
+                        <div class="author-text">
+                            <h4>lindamiku</h4>
+                            <h5>Marketing Manager</h5>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="swiper-slide testimonial-item">
+                    <i class="fas fa-quote-left"></i>
+                    <div class="testimonial-content">
+                        <p class="max-w750 p-large">"This theme is awesome and the designer is very
+                            helpful.
+                            I
+                            had a few questions before purchase. He/She helped me
+                            with
+                            all the doubts. Also, they provide quick support. Thank
+                            you
+                            so much for a beautiful theme"</p>
+                    </div>
+
+                    <div class="testimonial-author d-flex align-items-center">
+                        <div class="author">
+                            <img src=<?php echo site_url("assets/img/team/3.jpg") ?> alt="">
+                        </div>
+                        <div class="author-text">
+                            <h4>makrandpatne</h4>
+                            <h5>Web Designer</h5>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="swiper-slide testimonial-item">
+                    <i class="fas fa-quote-left"></i>
+                    <div class="testimonial-content">
+                        <p class="max-w750 p-large">"Basically im using this theme as a base for my
+                            new
+                            website
+                            and i couldnt be happier. Ran into some bumps but the
+                            team
+                            who developed this theme was there to help me with any
+                            problems. Very slick nice ajax portfolio theme"</p>
+                    </div>
+
+                    <div class="testimonial-author d-flex align-items-center">
+                        <div class="author">
+                            <img src=<?php echo site_url("assets/img/team/3.jpg") ?> alt="">
+                        </div>
+                        <div class="author-text">
+                            <h4>MTLGraphic</h4>
+                            <h5>Graphic Design</h5>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="control-nav p-absolute w-100  d-flex dsn-container align-items-center  justify-content-between ">
+            <div class="prev-container image-zoom move-circle background-theme" data-dsn="parallax">
+                <svg viewBox="0 0 40 40">
+                    <path class="path circle" d="M20,2A18,18,0,1,1,2,20,18,18,0,0,1,20,2">
+                    </path>
+                    <polyline class="path" points="14.6 17.45 20 22.85 25.4 17.45">
+                    </polyline>
+                </svg>
+            </div>
+            <div class="next-container image-zoom move-circle background-theme" data-dsn="parallax">
+                <svg viewBox="0 0 40 40">
+                    <path class="path circle" d="M20,2A18,18,0,1,1,2,20,18,18,0,0,1,20,2">
+                    </path>
+                    <polyline class="path" points="14.6 17.45 20 22.85 25.4 17.45">
+                    </polyline>
+                </svg>
+            </div>
+        </div>
+
+
+    </div>
+</section>
+<!-- ========== End testimonials 2 ========== -->
+
+<!-- ========== brand-client ========== -->
+<div class="dsn-container brand-client background-section section-padding" data-dsn-title="Our Clients">
+
+    <div class="section-title">
+        <span class="tag-heading p-10 mb-15 background-section heading-color">
+            Partenaires
+        </span>
+        <h2 class="heading-h2">
+            Nos différentes <br> partenaires
+        </h2>
+    </div>
+
+    <div class="wrapper-client d-grid grid-lg-3 grid-sm-2">
+        <div class="logo-box border">
+            <div class="logo-box-inner style-box">
+                <img src=<?php echo site_url("assets/img/logo/1.png") ?> alt="">
+            </div>
+        </div>
+        <div class="logo-box border">
+            <div class="logo-box-inner style-box">
+                <img src=<?php echo site_url("assets/img/logo/2.png") ?> alt="">
+            </div>
+        </div>
+        <div class="logo-box border">
+            <div class="logo-box-inner style-box">
+                <img src=<?php echo site_url("assets/img/logo/3.png") ?> alt="">
+            </div>
+        </div>
+        <div class="logo-box border">
+            <div class="logo-box-inner style-box">
+                <img src=<?php echo site_url("assets/img/logo/4.png") ?> alt="">
+            </div>
+        </div>
+        <div class="logo-box border">
+            <div class="logo-box-inner style-box">
+                <img src=<?php echo site_url("assets/img/logo/5.png") ?> alt="">
+            </div>
+        </div>
+        <div class="logo-box border">
+            <div class="logo-box-inner style-box">
+                <img src=<?php echo site_url("assets/img/logo/6.png") ?> alt="">
+            </div>
+        </div>
+
+    </div>
+</div>
+<!-- ========== End brand-client ========== -->
+
+<!-- ========== next-page ========== -->
+<!-- ========== End next-page ========== -->
+<div class="next-project h-v-100 section-top p-relative v-dark v-dark-head">
+    <div class="p-absolute w-100 h-100 bg-container over-hidden before-z-index" data-overlay="5">
+        <img class="cover-bg-img" alt="" src="data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==" data-dsn-src="assets/img/project/project2/1.jpg">
+    </div>
+    <div class="w-100 pt-30 d-flex justify-content-between dsn-container">
+        
+        <div class="scroll-arrow p-relative">
+            <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M3 13L17.17 13L13.59 16.59L15 18L21 12L15 6L13.59 7.41L17.17 11L3 11L3 13Z" fill="white"></path>
+            </svg>
+        </div>
+    </div>
+    <div class="dsn-container d-flex flex-column justify-content-center h-100">
+        <div class="footer-content">
+            <a href="#" class="effect-ajax has-box-mod move-circle" data-dsn="parallax" data-dsn-color="#284762" data-dsn-ajax="next">
+                <h2 class="title">
+                    Onirix, <br>les rêves deviennent réalités
+                </h2>
+            </a>
+            <div class="metas mt-25">
+                <span>IA</span>
+                <span>Webcup</span>
+            </div>
+        </div>
+    </div>
+</div>
