95f10e5075339d1371eba36b693af6addbf3a92c
syraxxxx <rsitraka181@gmail.com>
2023-05-07 00:45:48 +0300

    Update service_reponse.php

---
+++ application/views/service_reponse.php
@@ -0,0 +1,50 @@
+<div class="p-relative">
+    <div class="box-set-video bg-section p-absolute w-100 h-100 over-hidden top-0">
+        <div class="before-z-index h-100" data-dsn-grid="move-up" data-overlay="7">
+            <img class="p-absolute cover-bg-img has-top-bottom" src="data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==" data-dsn-src="assets/img/header.jpg" alt="" data-dsn-move="20%">
+        </div>
+    </div>
+    <div class=" dsn-container">
+        <div class="row">
+            <div class="col-xl-6 offset-xl-6 col-lg-7 offset-lg-5">
+                <div class="pro-text p-relative background-section box-padding z-index-1 section-margin">
+                    <div class="border-section-bottom mb-50">
+                        <h2 class="heading-h2">
+                            How is your <br> visual identity?
+                        </h2>
+                    </div>
+                    <div class="description max-w570">
+
+
+                        <p class="mb-10">
+                            A system that young people around the world with a club culture and
+                            techno
+                            enthusiasts
+                            feel identified.
+                            We generated a simple logo that is the basis for generating a geometric
+                            and
+                            liquid
+                            system.
+                        </p>
+
+                        <p>Founded in 2000, Dsn Grid has become one of the best Digital Agency
+                            in ThemeForest. Blue money going forward, but deploy to production.</p>
+                    </div>
+
+
+                    <div class="box-video mt-40 d-flex align-items-center">
+                        <a href="https://vimeo.com/175353205" class="vid c-hidden z-index-2">
+                            <div class="play-btn image-zoom background-main heading-color move-circle d-flex align-items-center justify-content-center" data-dsn="parallax"><i class="fas fa-play"></i>
+                            </div>
+                        </a>
+
+                        <h5 class="font-caps border-top border-bottom p-20">UNLIMITED POWER AND
+                            CUSTOMIZATION
+                        </h5>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
